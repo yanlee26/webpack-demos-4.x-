b1bafc73d58aa48b15eced377cbd356d7361ec41
yanlee26 <franklyan24@gmail.com>
2019-03-13 16:34:18 +0800

    async

--- 07-code-split/src/index.js
+++ 07-code-split/src/index.js
@@ -1,15 +1,20 @@
 
-function getComponent() {
-  return import ( /* webpackChunkName: "lodash" */ 'lodash').then(({
-    default: _
-  }) => {
-    var element = document.createElement('div');
+async function getComponent() {
+  // return import ( /* webpackChunkName: "lodash" */ 'lodash').then(({
+  //   default: _
+  // }) => {
+  //   var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+  //   return element;
 
-  }).catch(error => 'An error occurred while loading the component');
+  // }).catch(error => 'An error occurred while loading the component');
+
+  var element = document.createElement('div');
+  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
 }
 getComponent().then(component => {
   document.body.appendChild(component);
