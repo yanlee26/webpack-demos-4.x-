5953266745b868de5a86d09fe5c0d7d8f72ed53e
yanlee26 <franklyan24@gmail.com>
2019-03-13 16:29:47 +0800

    splitChunks

---
+++ 07-code-split/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+};
