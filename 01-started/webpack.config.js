6a944c39549248e08cc6def5449db28595c0f9e9
yanlee26 <franklyan24@gmail.com>
2020-01-29 21:00:31 +0800

    feat: 01 update

--- 01-started/webpack.config.js
+++ 01-started/webpack.config.js
@@ -3,6 +3,7 @@ const MyFirstPlugin = require('./MyFirstPlugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
